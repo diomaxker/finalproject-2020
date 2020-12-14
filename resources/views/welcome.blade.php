99a1280c7debfaf23c7eedf667d80a63c1cf4b4a
diomaxker <diomax.ker@gmail.com>
2020-12-14 21:12:38 +0800

    修改title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>柯伎璟的期末作業</title>
-
+        <hr>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
