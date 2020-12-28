f6c727bf7d6f5bfc1a708fa44e579e6fc58c42f2
diomaxker <diomax.ker@gmail.com>
2020-12-28 20:10:16 +0800

    增加文字

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-            <h1>J109212108柯伎璟的期末網站</h1>
+            <h1>J109212108的期末網站</h1>
             <hr>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
