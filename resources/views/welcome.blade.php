8c01e7250448d5bfeda9c8e8eb733d1f764266aa
diomaxker <diomax.ker@gmail.com>
2020-12-28 20:08:13 +0800

    增加文字

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>J109212108柯伎璟的期末作業</title>
+        <title>J109212108的期末作業</title>
         <hr>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
